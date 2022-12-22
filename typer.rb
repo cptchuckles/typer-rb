7a0fbdd2ae2080cf942b354ea890236818941c01
Grendel <jcg@grendel.world>
2022-12-21 18:32:19 -0600

    Initial commit

---
+++ typer.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+cachedir = ENV['XDG_CACHE'] || "#{ENV['HOME']}/.cache"
+
+files = `fd . "#{cachedir}/typefiles" -d1 -H -X realpath`.split
+
+file_lookup = {}
+files.group_by { |f| f.split('/').last }.each do |_, paths|
+  paths.map! { |path| path.split('/') }
+  elide = paths.first[..-2].intersection(*paths).length - 1
+  paths.each do |p|
+    file_lookup[p[elide..].join('/')] = p.join '/'
+  end
+end
+
+selection = IO.popen(['dmenu', '-p', 'Type file:'], 'r+') do |dmenu|
+  file_lookup.each_key do |short_path|
+    dmenu.puts short_path
+  end
+  dmenu.close_write
+  dmenu.gets
+end
+
+exit if selection.nil?
+selection.chomp!
+
+exit if file_lookup[selection].nil?
+
+`xdotool type --clearmodifiers --file #{file_lookup[selection]}`
